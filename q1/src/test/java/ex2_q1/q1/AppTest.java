25eb323cf60b7dcf17474bad93e3d52f88e373ca
wisam <wisam@DESKTOP-I22KP5J>
2024-01-27 15:31:28 +0200

    version 1

---
+++ q1/src/test/java/ex2_q1/q1/AppTest.java
@@ -0,0 +1,20 @@
+package ex2_q1.q1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
