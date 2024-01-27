d62346f69a50ee0fe26815212cd9f72832c8c797
wisam <wisam@DESKTOP-I22KP5J>
2024-01-27 16:33:24 +0200

    grade test

--- q1/src/main/java/ex2_q1/q1/App.java
+++ q1/src/main/java/ex2_q1/q1/App.java
@@ -6,6 +6,25 @@ package ex2_q1.q1;
  */
 public class App 
 {
+	public static String GradeTest(int grade) {
+        if (grade < 1) {
+            return "error";
+        } else if (grade >= 1 && grade <= 49 ) {
+            return "F";
+        } else if (grade > 49 && grade <= 59) {
+            return "E";
+        } else if (grade > 59 && grade <= 69) {
+            return "D";
+        } else if (grade > 69 && grade <= 79) {
+            return "C";
+        } else if (grade >79 && grade <= 89) {
+            return "B";
+        } else if (grade > 89 && grade <= 100) {
+            return "A";
+        } else {
+            return "error";
+        }
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
