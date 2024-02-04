f83d31c532ffb46143507be9bae9b6e5d42fec64
wisam <wisam@DESKTOP-I22KP5J>
2024-02-04 19:26:32 +0200

    i added the five bva tests

--- q1/src/test/java/ex2_q1/q1/AppTest.java
+++ q1/src/test/java/ex2_q1/q1/AppTest.java
@@ -1,5 +1,6 @@
 package ex2_q1.q1;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +14,38 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void B_partitionCheckMin() {
+    	String expected = "B";
+    	String result = App.GradeTest(80);
+    	assertEquals(expected, result);
+    	
+    }
+    @Test
+    public void B_partitionCheckMinPlusOne() {
+    	String expected = "B";
+    	String result = App.GradeTest(81);
+    	assertEquals(expected, result);
+    
+    }
+    @Test
+    public void B_partitionCheckNuminal() {
+    	String expected = "B";
+    	String result = App.GradeTest(85);
+    	assertEquals(expected, result);
+    
+    }
+    @Test
+    public void B_partitionCheckMaxMinusOne() {
+    	String expected = "B";
+    	String result = App.GradeTest(88);
+    	assertEquals(expected, result);
+    
+    }
+    @Test
+    public void B_partitionCheckMax() {
+    	String expected = "B";
+    	String result = App.GradeTest(89);
+    	assertEquals(expected, result);
+    
     }
 }
